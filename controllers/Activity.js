18bec51b33977f03961851885af0103c47c5660a
ALI-TWG <muhammadali.twg2@gmail.com>
2024-08-30 03:56:38 +0500

    changes

--- controllers/Activity.js
+++ controllers/Activity.js
@@ -3,7 +3,14 @@ const ActivityLog = require("../models/activity");
 // Get all activity logs
 const getActivityLogs = async (req, res) => {
   try {
-    const activityLogs = await ActivityLog.find();
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 50;
+    const skip = (page - 1) * limit;
+    const activityLogs = await ActivityLog.find()
+      .sort([["createdAt", "descending"]])
+      .skip(skip)
+      .limit(limit);
+
     // console.log(activityLogs);
     res.status(200).json({ data: activityLogs });
   } catch (error) {
