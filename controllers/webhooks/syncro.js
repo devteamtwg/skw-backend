abdfe258dcd45b382310080b5b1a2663f7efc862
ALI-TWG <muhammadali.twg2@gmail.com>
2024-09-25 08:05:02 +0500

    changes

--- controllers/webhooks/syncro.js
+++ controllers/webhooks/syncro.js
@@ -5,6 +5,7 @@ const ActivityLog = require("../../models/activity");
 
 // Customer Created in Syncro
 const handleCustomerCreation = async (req, res) => {
+  console.log("/////*******************************************/////");
   const date = new Date();
   console.log(
     `Customer Created in Syncro at ${date.toLocaleTimeString()}`,
@@ -25,6 +26,10 @@ const handleCustomerCreation = async (req, res) => {
     res.status(404).send("Location not found!");
     return;
   }
+
+  const client = await Client.findOne({
+    user_id: customerLocation.user_id,
+  });
 
   // Refresh Highlvel Access Token
   let new_access_token;
@@ -64,11 +69,6 @@ const handleCustomerCreation = async (req, res) => {
     console.log(error);
   }
 
-  // Get Business
-  const client = await Client.findOne({
-    user_id: customerLocation.user_id,
-  });
-
   const payload = {
     email: syncroCustomer.attributes.email,
     phone: syncroCustomer.attributes.phone,
@@ -85,20 +85,91 @@ const handleCustomerCreation = async (req, res) => {
   // console.log("payload", payload);
 
   try {
-    const duplicateCustomerRes = await axios.get(
-      `https://services.leadconnectorhq.com/contacts/search/duplicate?locationId=${
-        customerLocation.hl_location_id
-      }&email=${encodeURIComponent(syncroCustomer.email)}`,
-      {
-        headers: {
-          Authorization: `Bearer ${new_access_token}`,
-          Version: "2021-07-28",
-        },
+    let duplicateCustomerRes =
+      payload.email &&
+      (await axios.get(
+        `https://services.leadconnectorhq.com/contacts/search/duplicate?locationId=${
+          customerLocation.hl_location_id
+        }&email=${encodeURIComponent(syncroCustomer.email)}`,
+        {
+          headers: {
+            Authorization: `Bearer ${new_access_token}`,
+            Version: "2021-07-28",
+          },
+        }
+      ));
+
+    // If no customer is found by email, check by phone
+    if (
+      duplicateCustomerRes == undefined ||
+      duplicateCustomerRes.data.contact == null
+    ) {
+      if (payload.phone) {
+        duplicateCustomerRes = await axios.get(
+          `https://services.leadconnectorhq.com/contacts/search/duplicate?locationId=${
+            customerLocation.hl_location_id
+          }&number=${encodeURIComponent(payload.phone)}`,
+          {
+            headers: {
+              Authorization: `Bearer ${new_access_token}`,
+              Version: "2021-07-28",
+            },
+          }
+        );
       }
-    );
+    }
+
     console.log("duplicateCustomerRes", duplicateCustomerRes.data);
 
-    if (duplicateCustomerRes.data.contact == null) {
+    if (duplicateCustomerRes.data.contact != null) {
+      delete payload.locationId;
+      try {
+        const updateHighlevelCustomerRes = await axios.put(
+          `https://services.leadconnectorhq.com/contacts/${duplicateCustomerRes.data.contact.id}`,
+          payload,
+          {
+            headers: {
+              Authorization: `Bearer ${new_access_token}`,
+              Version: "2021-07-28",
+            },
+          }
+        );
+        console.log(
+          "Syncro Customer Synced with Existing Highlevel Customer Successfully",
+          updateHighlevelCustomerRes.data
+        );
+
+        // Log success
+        await ActivityLog.create({
+          user_id: customerLocation.user_id,
+          businessName: client.business_name,
+          eventType: "Success",
+          event: "Customer Created in Syncro",
+          platform: "Syncro",
+          message: `Customer <b>${
+            payload.email || payload.phone
+          }</b> Synced with Existing Highlevel Customer Successfully`,
+          customData: updateHighlevelCustomerRes.data,
+        });
+      } catch (error) {
+        console.error(error.response);
+
+        // Log failure
+        await ActivityLog.create({
+          user_id: customerLocation.user_id,
+          businessName: client.business_name,
+          eventType: "Failure",
+          event: "Customer Created in Syncro",
+          platform: "Syncro",
+          message: `Error syncing Syncro customer with Existing Highlevel Customer ${
+            error.response ? error.response.data.message : ""
+          }`,
+          customData: error.response ? error.response.data : error,
+        });
+      }
+    }
+    // If not exists, Create one
+    else {
       try {
         const highlevelCustomerRes = await axios.post(
           "https://services.leadconnectorhq.com/contacts/",
@@ -111,15 +182,20 @@ const handleCustomerCreation = async (req, res) => {
           }
         );
         console.log(
-          "Syncro Customer Synced with Highlevel Successfully",
+          "Syncro Customer Synced in Highlevel Successfully",
           highlevelCustomerRes.data
         );
 
         // Log success
         await ActivityLog.create({
           user_id: customerLocation.user_id,
+          businessName: client.business_name,
           eventType: "Success",
-          message: `Syncro Customer Synced with Highlevel Successfully`,
+          event: "Customer Created in Syncro",
+          platform: "Syncro",
+          message: `Customer <b>${
+            payload.email || payload.phone
+          }</b> Synced in Highlevel Successfully`,
           customData: highlevelCustomerRes.data,
         });
       } catch (error) {
@@ -128,7 +204,10 @@ const handleCustomerCreation = async (req, res) => {
         // Log failure
         await ActivityLog.create({
           user_id: customerLocation.user_id,
+          businessName: client.business_name,
           eventType: "Failure",
+          event: "Customer Created in Syncro",
+          platform: "Syncro",
           message: `Error syncing Syncro customer in Highlevel`,
           customData: error.response ? error.response.data : error,
         });
@@ -139,10 +218,12 @@ const handleCustomerCreation = async (req, res) => {
   }
 
   res.status(200).send("Webhook received successfully");
+  console.log("/////*******************************************/////");
 };
 
 // Ticket Status changed in Syncro
 const handleTicketStatusChanged = async (req, res) => {
+  console.log("/////*******************************************/////");
   const date = new Date();
   console.log(
     `Ticket Status Changed in Syncro at ${date.toLocaleTimeString()}`,
@@ -164,6 +245,10 @@ const handleTicketStatusChanged = async (req, res) => {
     res.status(404).send("Location not found!");
     return;
   }
+
+  const client = await Client.findOne({
+    user_id: customerLocation.user_id,
+  });
 
   // Refresh Highlvel Access Token
   let new_access_token;
@@ -211,11 +296,20 @@ const handleTicketStatusChanged = async (req, res) => {
         page: 1,
         pageLimit: 20,
         filters: [
-          {
-            field: "email",
-            operator: "eq",
-            value: customer.email,
-          },
+          customer.email
+            ? {
+                field: "email",
+                operator: "eq",
+                value: customer.email,
+              }
+            : {
+                field: "phone",
+                operator: "eq",
+                value:
+                  customer?.phone?.replace(/[\s-]+/g, "") ||
+                  customer?.mobile?.replace(/[\s-]+/g, "") ||
+                  "",
+              },
         ],
       },
       {
@@ -245,15 +339,22 @@ const handleTicketStatusChanged = async (req, res) => {
           }
         );
         console.log(
-          `${status} Tag added in Highlevel's customer ${customer.email}`,
+          `${status} Tag added in Highlevel's customer ${
+            customer.email || customer.phone || customer.mobile
+          }`,
           addTagRes.data
         );
 
         // Log success
         await ActivityLog.create({
           user_id: customerLocation.user_id,
+          businessName: client.business_name,
           eventType: "Success",
-          message: `Tag added in Highlevel's customer ${customer.email}`,
+          event: "Ticket Status Changed in Syncro",
+          platform: "Syncro",
+          message: `Tag added in Highlevel's customer <b>${
+            customer.email || customer.phone || customer.mobile
+          }</b>`,
           customData: addTagRes.data,
         });
       } catch (error) {
@@ -262,8 +363,11 @@ const handleTicketStatusChanged = async (req, res) => {
         // Log failure
         await ActivityLog.create({
           user_id: customerLocation.user_id,
+          businessName: client.business_name,
           eventType: "Failure",
-          message: `Error adding tag in Highlevel's Customer ${customer.email}`,
+          event: "Ticket Status Changed in Syncro",
+          platform: "Syncro",
+          message: `Error adding tag in Highlevel's Customer`,
           customData: error.response ? error.response.data : error,
         });
       }
@@ -274,18 +378,21 @@ const handleTicketStatusChanged = async (req, res) => {
     // Log failure
     await ActivityLog.create({
       user_id: customerLocation.user_id,
+      businessName: client.business_name,
       eventType: "Failure",
-      message: `Error adding tag in Highlevel's Customer ${customer.email}`,
+      event: "Ticket Status Changed in Syncro",
+      platform: "Syncro",
+      message: `Error adding tag in Highlevel's Customer`,
       customData: error.response ? error.response.data : error,
     });
   }
-
-  // Create a tag in Highlevel
   res.status(200).send("Webhook received successfully");
+  console.log("/////*******************************************/////");
 };
 
 // Invoice is Paid in Syncro
 const handleInvoicePaid = async (req, res) => {
+  console.log("/////*******************************************/////");
   // const date = new Date();
   // console.log(
   //   `An Invoice is Paid in Syncro at ${date.toLocaleTimeString()}`,
@@ -307,6 +414,10 @@ const handleInvoicePaid = async (req, res) => {
     res.status(404).send("Location not found!");
     return;
   }
+
+  const client = await Client.findOne({
+    user_id: customerLocation.user_id,
+  });
 
   // Refresh Highlvel Access Token
   let new_access_token;
@@ -354,11 +465,20 @@ const handleInvoicePaid = async (req, res) => {
         page: 1,
         pageLimit: 20,
         filters: [
-          {
-            field: "email",
-            operator: "eq",
-            value: customer.email,
-          },
+          customer.email
+            ? {
+                field: "email",
+                operator: "eq",
+                value: customer.email,
+              }
+            : {
+                field: "phone",
+                operator: "eq",
+                value:
+                  customer?.phone?.replace(/[\s-]+/g, "") ||
+                  customer?.mobile?.replace(/[\s-]+/g, "") ||
+                  "",
+              },
         ],
       },
       {
@@ -389,7 +509,7 @@ const handleInvoicePaid = async (req, res) => {
         // console.log(addTagRes.data);
         console.log(
           `${success && "Invoice Paid"} Tag added in Highlevel's customer ${
-            customer.email
+            customer.email || customer.phone || customer.mobile
           }`,
           addTagRes.data
         );
@@ -397,10 +517,15 @@ const handleInvoicePaid = async (req, res) => {
         // Log success
         await ActivityLog.create({
           user_id: customerLocation.user_id,
+          businessName: client.business_name,
           eventType: "Success",
+          event: "Invoice Paid in Syncro",
+          platform: "Syncro",
           message: `${
             success && "Invoice Paid"
-          } Tag added in Highlevel's customer ${customer.email}`,
+          } Tag added in Highlevel's customer <b>${
+            customer.email || customer.phone || customer.mobile
+          }</b>`,
           customData: addTagRes.data,
         });
       } catch (error) {
@@ -409,8 +534,11 @@ const handleInvoicePaid = async (req, res) => {
         // Log failure
         await ActivityLog.create({
           user_id: customerLocation.user_id,
+          businessName: client.business_name,
           eventType: "Failure",
-          message: `Error adding tag in Highlevel's Customer ${customer.firstName}`,
+          event: "Invoice Paid in Syncro",
+          platform: "Syncro",
+          message: `Error adding tag in Highlevel's Customer`,
           customData: error.response ? error.response.data : error,
         });
       }
@@ -421,14 +549,18 @@ const handleInvoicePaid = async (req, res) => {
     // Log failure
     await ActivityLog.create({
       user_id: customerLocation.user_id,
+      businessName: client.business_name,
       eventType: "Failure",
-      message: `Error adding tag in Highlevel's Customer ${customer.firstName}`,
+      event: "Invoice Paid in Syncro",
+      platform: "Syncro",
+      message: `Error adding tag in Highlevel's Customer`,
       customData: error.response ? error.response.data : error,
     });
   }
 
   // Create a tag in Highlevel
   res.status(200).send("Webhook received successfully");
+  console.log("/////*******************************************/////");
 };
 
 module.exports = {
